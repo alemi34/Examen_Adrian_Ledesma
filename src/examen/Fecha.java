e99ddd66b141d9dd80928902275e1b6e6b4b7b29
alemi34 <2004alemi@gmail.com>
2023-02-22 09:50:53 +0100

    Proyecto Documentado

--- src/examen/Fecha.java
+++ src/examen/Fecha.java
@@ -1,6 +1,25 @@
 package examen;
 
+/**
+ * Lo que hace este programa es comprobar si una fecha es valida
+ * 
+ * @author dam108
+ * @version 1.0
+ *
+ */
 public class Fecha {
+	/**
+	 * Lo que hace es devolver si la fecha es valida o no dependiendo los valores que se ingresen 
+	 * 
+	 * @param anio
+	 * 			Este dato que se ingresa nos indica el año que queremos 
+	 * @param mes
+	 * 			Este dato que se ingresa nos indica el mes que queremos
+	 * @param dia
+	 * 			Este dato que se ingresa nos indica el dia que queremos 
+	 * @return
+	 * 			Devuelve si la fecha es valida
+	 */
 	public static boolean validarFecha(int anio, int mes, int dia) {
 		boolean fechaValida = false;
 		if ((dia > 0) && (mes > 0) && (anio > 0)) {
